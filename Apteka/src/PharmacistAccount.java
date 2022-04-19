4a444def7f47d078bdbe483ef2656d81ffdf03eb
przecier32 <102722564+przecier32@users.noreply.github.com>
2022-04-19 22:05:52 +0200

    Add files via upload

---
+++ Apteka/src/PharmacistAccount.java
@@ -0,0 +1,12 @@
+public class PharmacistAccount {
+
+    static public void printPharm() {
+        System.out.println("eeeeee");
+        System.out.println("eeeeee");
+        System.out.println("eeeeee");
+        System.out.println("eeeeee");
+        System.out.println("eeeeee");
+        System.out.println("eeeeee");
+    }
+
+}
